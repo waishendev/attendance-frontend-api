784745986a0ada12cec39ede3e92521283a91117
waishendev <149239484+waishendev@users.noreply.github.com>
2025-09-05 19:32:15 +0800

    Add attendance controller and models

---
+++ app/Models/AttendanceLog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'work_date',
+        'status',
+        'check_in_at',
+        'check_out_at',
+    ];
+
+    protected $casts = [
+        'work_date' => 'date',
+        'check_in_at' => 'datetime',
+        'check_out_at' => 'datetime',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
